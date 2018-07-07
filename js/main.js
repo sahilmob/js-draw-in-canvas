cc982bd0f330d01199c5a3e00d2a8c1435ddc580
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-07-07 17:16:17 +0300

    Add logic to set the pen x and y values to equal the mouse click event offsetX and offsetY values

--- js/main.js
+++ js/main.js
@@ -1,32 +1,34 @@
-const canvas = document.querySelector('#canvas')
-const ctx = canvas.getContext('2d')
-const penColor = document.querySelector('input[name="penColor"]')
-const penWidth = document.querySelector('input[name="penWidth"]')
-const saver = document.querySelector('#saver')
+const canvas = document.querySelector('#canvas');
+const ctx = canvas.getContext('2d');
+const penColor = document.querySelector('input[name="penColor"]');
+const penWidth = document.querySelector('input[name="penWidth"]');
+const saver = document.querySelector('#saver');
 
-ctx.strokeStyle = '#000000'
+ctx.strokeStyle = '#000000';
 ctx.lineJoin = 'round'
-ctx.lineCap = 'round'
-ctx.lineWidth = '5'
+ctx.lineCap = 'round';
+ctx.lineWidth = '5';
 
 let pen = {
     x: 0,
     y: 0,
     down: false
-}
+};
 
-saver.addEventListener('click', saveFile)
-canvas.addEventListener('mousedown', penDown)
-canvas.addEventListener('mousemove', draw)
-canvas.addEventListener('mouseup', penUp)
-canvas.addEventListener('mouseout', penUp)
+saver.addEventListener('click', saveFile);
+canvas.addEventListener('mousedown', penDown);
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mouseup', penUp);
+canvas.addEventListener('mouseout', penUp);
 
 function saveFile() {
 
 }
 
-function penDown() {
-
+function penDown(e) {
+    pen.down = true;
+    [pen.x, pen.y] = [e.offsetX, e.offsetY];
+    console.log(pen);
 }
 
 function draw() {
@@ -34,5 +36,5 @@ function draw() {
 }
 
 function penUp() {
-
+    pen.down = false;
 }
